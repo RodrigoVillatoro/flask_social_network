b613e993e85ac137980a79ea2c5a80243dd4afe4
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-01-29 15:39:01 +0100

    getting ready for heroku

--- config.py
+++ config.py
@@ -77,9 +77,24 @@ class ProductionConfig(Config):
         app.logger.addHandler(mail_handler)
 
 
+class HerokuConfig(ProductionConfig):
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stdere
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig,
+    'heroku': HerokuConfig,
 }
