46d9fa056691fdc12dfa0935a3c52c981ec02eb1
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-01-29 16:03:56 +0100

    fixed config

--- config.py
+++ config.py
@@ -1,23 +1,22 @@
 import os
 
-import config_secret as cs
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     DEBUG = False
     TESTING = False
-    SECRET_KEY = cs.SECRET_KEY
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     APP_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    APP_MAIL_SENDER = cs.APP_MAIL_SENDER
-    MAIL_USERNAME = cs.MAIL_USERNAME
-    MAIL_PASSWORD = cs.MAIL_PASSWORD
-    MAIL_SERVER = cs.MAIL_SERVER
-    MAIL_PORT = cs.MAIL_PORT
-    APP_ADMIN = cs.APP_ADMIN
+    APP_MAIL_SENDER = 'Flasky Admin'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    APP_ADMIN = os.environ.get('FLASKY_ADMIN')
     APP_POSTS_PER_PAGE = 20
     APP_FOLLOWERS_PER_PAGE = 20
     APP_FOLLOWING_PER_PAGE = 20
@@ -32,11 +31,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = cs.MAIL_SERVER
-    MAIL_PORT = cs.MAIL_PORT
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = cs.MAIL_USERNAME
-    MAIL_PASSWORD = cs.MAIL_PASSWORD
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
